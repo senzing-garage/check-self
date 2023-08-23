38444bc497f28283814828e9271f65aa3eba4dcf
docktermj <michael@dockter.com>
2023-08-23 16:55:07 -0400

    #3 Modify test data - 7

--- checkself/checkself_windows_test.go
+++ checkself/checkself_windows_test.go
@@ -25,6 +25,18 @@ func TestCheckSelfImpl_CheckSelf_Paths(test *testing.T) {
 	assert.Nil(test, err)
 }
 
+func TestCheckSelfImpl_CheckSelf_Paths4(test *testing.T) {
+	ctx := context.TODO()
+	testObject := &CheckSelfImpl{
+		ConfigPath:   `C:\Program Files\Senzing\g2\etc`,
+		DatabaseUrl:  `sqlite3://nowhere.com:0/C:\Temp\sqlite\G2C.db`,
+		ResourcePath: `C:\Program Files\Senzing\g2\resources`,
+		SupportPath:  `C:\Program Files\Senzing\g2\data`,
+	}
+	err := testObject.CheckSelf(ctx)
+	assert.Nil(test, err)
+}
+
 func TestCheckSelfImpl_CheckSelf_Paths1(test *testing.T) {
 	ctx := context.TODO()
 	testObject := &CheckSelfImpl{
