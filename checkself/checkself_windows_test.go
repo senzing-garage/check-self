dff3c06030e437f29d86c0060c914a3b8899c754
docktermj <michael@dockter.com>
2023-08-23 16:40:35 -0400

    #3 Modify test data - 6

--- checkself/checkself_windows_test.go
+++ checkself/checkself_windows_test.go
@@ -29,7 +29,7 @@ func TestCheckSelfImpl_CheckSelf_Paths1(test *testing.T) {
 	ctx := context.TODO()
 	testObject := &CheckSelfImpl{
 		ConfigPath:   `C:\Program Files\Senzing\g2\etc`,
-		DatabaseUrl:  `sqlite3://C:\Temp\sqlite\G2C.db`,
+		DatabaseUrl:  `sqlite3://na:na@nowhere.com:0/C:\Temp\sqlite\G2C.db`,
 		ResourcePath: `C:\Program Files\Senzing\g2\resources`,
 		SupportPath:  `C:\Program Files\Senzing\g2\data`,
 	}
@@ -41,7 +41,7 @@ func TestCheckSelfImpl_CheckSelf_Paths2(test *testing.T) {
 	ctx := context.TODO()
 	testObject := &CheckSelfImpl{
 		ConfigPath:   `C:\Program Files\Senzing\g2\etc`,
-		DatabaseUrl:  `sqlite3:///C:\Temp\sqlite\G2C.db`,
+		DatabaseUrl:  `sqlite3://na:na@nowhere:0/C:\Temp\sqlite\G2C.db`,
 		ResourcePath: `C:\Program Files\Senzing\g2\resources`,
 		SupportPath:  `C:\Program Files\Senzing\g2\data`,
 	}
@@ -53,7 +53,7 @@ func TestCheckSelfImpl_CheckSelf_Paths3(test *testing.T) {
 	ctx := context.TODO()
 	testObject := &CheckSelfImpl{
 		ConfigPath:   `C:\Program Files\Senzing\g2\etc`,
-		DatabaseUrl:  `sqlite3://example.com/C:\Temp\sqlite\G2C.db`,
+		DatabaseUrl:  `sqlite3://na:na@nowhere/C:\Temp\sqlite\G2C.db`,
 		ResourcePath: `C:\Program Files\Senzing\g2\resources`,
 		SupportPath:  `C:\Program Files\Senzing\g2\data`,
 	}
