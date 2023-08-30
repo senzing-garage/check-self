c794e430d1f99b1c675e4d7e9183b7476fe426c0
docktermj <michael@dockter.com>
2023-08-30 16:19:01 -0400

    #3 Add windows testing - 2

--- checkself/checkself_windows_test.go
+++ checkself/checkself_windows_test.go
@@ -17,7 +17,7 @@ func TestCheckSelfImpl_CheckSelf_Paths(test *testing.T) {
 	ctx := context.TODO()
 	testObject := &CheckSelfImpl{
 		ConfigPath:   `C:\Program Files\Senzing\g2\etc`,
-		DatabaseUrl:  `sqlite3://na:na@/C:\Temp\sqlite\G2C.db`,
+		DatabaseUrl:  `sqlite3://na:na@nowhere/C:\Temp\sqlite\G2C.db`,
 		ResourcePath: `C:\Program Files\Senzing\g2\resources`,
 		SupportPath:  `C:\Program Files\Senzing\g2\data`,
 	}
