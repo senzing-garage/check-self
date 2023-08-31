ebb9b0866820756eecb6cfa755625bd00f5cc363
docktermj <michael@dockter.com>
2023-08-31 10:54:12 -0400

    #3 Add windows testing - 13

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("sqlite3://na:na@nowhere/C:\\Temp\\sqlite\\G2C.db")
-	fmt.Print(`sqlite3://na:na@nowhere/C:\\Temp\\sqlite\\G2C.db`)
-	fmt.Print(`sqlite3://na:na@nowhere/C:\Temp\sqlite\G2C.db`)
+	fmt.Println("sqlite3://na:na@nowhere/C:\\Temp\\sqlite\\G2C.db")
+	fmt.Println(`sqlite3://na:na@nowhere/C:\\Temp\\sqlite\\G2C.db`)
+	fmt.Println(`sqlite3://na:na@nowhere/C:\Temp\sqlite\G2C.db`)
 }
