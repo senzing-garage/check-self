11af21324e23bad03c4dc14a012a34154167fbf5
docktermj <michael@dockter.com>
2025-02-27 14:53:52 -0500

    Fix code

--- checkself/checkself_windows_test.go
+++ checkself/checkself_windows_test.go
@@ -4,8 +4,10 @@ package checkself
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
+	"github.com/senzing-garage/go-helpers/settings"
 	"github.com/stretchr/testify/require"
 )
 
@@ -17,10 +19,10 @@ func TestBasicCheckSelf_CheckSelf_Paths(test *testing.T) {
 	ctx := context.TODO()
 	senzingPath := settings.GetSenzingPath()
 	testObject := &BasicCheckSelf{
-		ConfigPath:   fmt.Sprintf("%s\\er\etc", senzingPath),
+		ConfigPath:   fmt.Sprintf("%s\\er\\etc", senzingPath),
 		DatabaseURL:  "sqlite3://na:na@nowhere/C:\\Temp\\sqlite\\G2C.db",
-		ResourcePath: fmt.Sprintf("%s\\er\resources", senzingPath),
-		SupportPath:  fmt.Sprintf("%s\\er\data", senzingPath),
+		ResourcePath: fmt.Sprintf("%s\\er\\resources", senzingPath),
+		SupportPath:  fmt.Sprintf("%s\\er\\data", senzingPath),
 	}
 	err := testObject.CheckSelf(ctx)
 	require.NoError(test, err)
